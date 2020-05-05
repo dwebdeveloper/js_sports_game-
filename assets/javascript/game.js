1a24f7e1d9a4440bb5baaf8c630aa5b430aef87a
James Farfan <jamesrojas710@gmail.com>
2020-05-05 00:04:44 -0400

    new files added

---
+++ assets/javascript/game.js
@@ -0,0 +1,4 @@
+const resetButton = document.querySelector("reset-button");
+const teamoneShootButton = document.querySelector("#teamone-shoot-button");
+const teamtwoShootButton = document.querySelector("teamtwo-shoot-button");
+
