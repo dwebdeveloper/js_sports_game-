d7233af4fa08ddd7868d78f959d0386c75175ece
James Farfan <jamesrojas710@gmail.com>
2020-05-06 00:38:32 -0400

    assets/javascript/game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,33 +7,35 @@ let teamoneNumshots = document.querySelector("#teamone-numshots");
 let teamoneNumgoals = document.querySelector("#teamone-numgoals");
 //Below T2 IDs listed: Shots and Goals
 let teamtwoNumshots = document.querySelector("#teamtwo-numshots");
-let teamtwoNumgoals = document.querySelector("#teamtwo-numshots");
+let teamtwoNumgoals = document.querySelector("#teamtwo-numgoals");
 //Below: response and call of function
 teamoneShootButton.addEventListener("click", function () {
     console.log("+ button clicked");
+    let goal = Math.random();
     let t1shots = Number(teamoneNumshots.innerHTML) + 1;
     teamoneNumshots.innerHTML = t1shots;
-
-})
-let goal = Math.random();
-teamoneShootButton.addEventListener("click", function () {
-    if (goal < ) {
+    if (goal < .5) {
         let t1Goals = Number(teamoneNumgoals.innerHTML) + 1;
-        teamoneNumgoals = t1Goals;
-        console.log("Successful Goal")
+        teamoneNumgoals.innerHTML = t1Goals;
+        console.log("Successful Goal");
     }
-
 })
-
 
 teamtwoShootButton.addEventListener("click", function () {
-
+    let goals = Math.random();
     console.log("+ button clicked");
-    let t2Shots = Number(teamtwoNumshots.innerHTML) + 1;
-    teamtwoNumshots.innerHTML = t2Shots;
+    let t2shots = Number(teamtwoNumshots.innerHTML) + 1;
+    teamtwoNumshots.innerHTML = t2shots;
+    if (goals < .5) {
+        let t2Goals = Number(teamtwoNumgoals.innerHTML) + 1;
+        teamtwoNumgoals.innerHTML = t2Goals;
+        console.log("T2 goals");
+    }
 })
-
+let numResets = document.querySelector("#num-resets");
 resetButton.addEventListener("click", function () {
-    console.log("reset complete");
+    console.log("Resets completed")
+    let reset = Number(numResets.innerHTML) + 1;
+    numResets.innerHTML = reset;
 
 })
